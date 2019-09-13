5e14bb89d65e75ea0bff5cc441bed2ede2f7a2b7
Shawn Polson <sapols@yahoo.com>
2019-09-13 14:18:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,62 @@
-var WebSocketServer = require("ws").Server
+const WebSocket = require('ws');
+const ip = require('ip');
 var http = require("http")
 var express = require("express")
 var app = express()
-var port = process.env.PORT || 5000
+let serverPort = process.env.PORT;
+if (serverPort == null || serverPort == "") {
+  serverPort = 5000 //8000;
+}
+let serverIP = ip.address();
 
 app.use(express.static(__dirname + "/"))
 
-var server = http.createServer(app)
-server.listen(port)
+// var server = http.createServer(app)
+// server.listen(serverPort)
 
-console.log("http server listening on %d", port)
+//console.log("http server listening on %d", serverPort)
 
-var wss = new WebSocketServer({server: server})
+//var wss = new WebSocketServer({server: server})
+//var wss = new WebSocketServer({ server: server }, () => {
+const wss = new WebSocket.Server({ port: serverPort }, () => {
+   console.log(`Shawn's new WebSocket Server is running! (URL: ws://${serverIP}:${serverPort})`);
+   console.log("The signaling server is now listening on port " + serverPort);
+});
 console.log("websocket server created")
 
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+// wss.on("connection", function(ws) {
+//   var id = setInterval(function() {
+//     ws.send(JSON.stringify(new Date()), function() {  })
+//   }, 1000)
 
-  console.log("websocket connection open")
+//   console.log("websocket connection open")
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+//   ws.on("close", function() {
+//     console.log("websocket connection close")
+//     clearInterval(id)
+//   })
+// })
+
+// Broadcast to all.
+wss.broadcast = (ws, data) => {
+    wss.clients.forEach((client) => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    });
+};
+
+wss.on('connection', (ws, req) => {
+    //console.log(`Client with IP ${req.connection.remoteAddress} connected. Total connected clients: ${wss.clients.size}`);
+    console.log(`Client connected.`);
+
+    ws.onmessage = (message) => {
+        console.log(message.data + "\n");
+        wss.broadcast(ws, message.data);
+    }
+
+    ws.onclose = () => {
+        //console.log(`Client with IP ${req.connection.remoteAddress} disconnected. Total connected clients: ${wss.clients.size}`)
+        console.log(`Client disconnected.`)
+    }
+});
